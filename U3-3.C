964f6bba07b94c8a75fbc7313863fddc89dd0a24
princejadeja1881-dev <princejadeja1881@gmail.com>
2026-08-07 10:53:02 +0530

    Add files via upload

---
+++ U3-3.C
@@ -0,0 +1,29 @@
+//wap to interchange two numbers without using third veriable
+#include<stdio.h>
+#include<conio.h>
+
+void main ()
+
+{
+	int x;
+
+	clrscr();
+
+	printf("\n enter your age");
+	scanf("%d",&x);
+
+	if(x>=18)
+	{
+
+		printf("\n your are eligible for vote");
+	}
+	else
+	{
+		printf("\n ypur are not eligible for vote");
+
+	}
+	getch();
+}
+
+
+
