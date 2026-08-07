3c8c39e17b71393f1b749293758e925a053a541c
princejadeja1881-dev <princejadeja1881@gmail.com>
2026-08-07 10:00:52 +0530

    Add files via upload

---
+++ U3-2.C
@@ -0,0 +1,30 @@
+//wap to interchange two numbers without using third veriable
+#include<stdio.h>
+#include<conio.h>
+
+void main ()
+
+{
+	int x,y;
+
+
+	clrscr();
+
+	printf("\n enter the value of x :");
+	scanf("%d",&x);
+
+	printf("\n enter the value of y :");
+	scanf("%d",&y);
+
+	if(x<y)
+	{
+		printf("\n value of x is Minimum");
+	}
+	else
+	{
+		printf("\n value of y is Minimum");
+	}
+
+	getch();
+
+}
